7dce27f6aae1f73e00d3075636b0c2f0f3a90034
mbartoli <bartolimichael@gmail.com>
2015-09-05 22:32:42 -0700

    working grayscale

--- Experiment01/experiment_01.m
+++ Experiment01/experiment_01.m
@@ -1,3 +1,5 @@
+%% @author Mike Bartoli
+%% 
 %% Clean up
 % These functions calls clean up the MATLAB environment and close all windows
 % open "extra" windows.
@@ -9,14 +11,14 @@ close all
 % The next few lines define variables for the locations and types of image files
 % we will be reading and writing. You  will likely want to change the input and
 % output directories to match you personal environment.
-input_dir = '/common/cs/cs181m/data/eppstein';
-output_dir = '~/Downloads/';
+input_dir = '/home/m/git/cpe/data/eppstein/';
+output_dir = '/home/m/git/cpe/Experiment01/';
 file_ext = 'jpg';
 % Get a list of the jpg files in the input directory
-file_names = dir([input_dir '*.' file_ext]); 
+file_names = dir([input_dir '*']); 
 % Get the name of the 6th file in list. You should feel change this value.
-file_name = file_names(6).name; 
-
+file_name = file_names(3).name; 
+display(file_names(4).name)
 
 %% Read image file
 % Here we read the input jpg file into a 3D array of 8-bit integers. Before we
@@ -62,18 +64,34 @@ imwrite(B, [output_dir file_name(1:end - length(file_ext) - 1) '_blue.' file_ext
 
 
 %% Compute gray scale images
-% TODO: Store mean value result in I_M
-% TODO: Store weighted mean value in I_WM
-% TODO: Store desaturation in I_D
+% Store mean value result in I_M
+I_M = (R + G + B) / 3
 
+% Store weighted mean value in I_WM
+I_WM = 0.3 * R + 0.59 * G + 0.11 * B
+
+% Store desaturation in I_D
+I_D = 0.5*max(R,max(G,B)) + 0.5*min(R,min(G,B))
 
 %% Display gray scale conversions
-% TODO: Display all three gray scale conversions in single figure window
-%       using subplots, as we did with the color channels above.
-
+%  Display all three gray scale conversions in single figure window
+%  using subplots, as we did with the color channels above.
+figure
+subplot(1,3,1)
+imshow(I_M)
+title('Mean Value')
+subplot(1,3,2)
+imshow(I_WM)
+title('Weighted Mean Value')
+subplot(1,3,3)
+imshow(I_D)
+title('Desaturation')
 
 %% Write gray scale conversions
-% TODO: Write the three gray scale conversions to files, as we did with the
-%       color channels.
+%  Write the three gray scale conversions to files, as we did with the
+%  color channels.
+imwrite(I_M, [output_dir file_name(1:end - length(file_ext) - 1) '_M.' file_ext])
+imwrite(I_WM, [output_dir file_name(1:end - length(file_ext) - 1) '_WM.' file_ext])
+imwrite(I_D, [output_dir file_name(1:end - length(file_ext) - 1) '_D.' file_ext])
 
 
